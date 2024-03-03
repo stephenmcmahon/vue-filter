155d9d67d1798377983aab11a83935a38ab1e604
Stephen McMahon <steve.mcmahons@gmail.com>
2024-03-03 14:53:15 -0500

    working framework

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -5,19 +5,21 @@
     { id: 1, title: "Brand 1", sort: "100", code: "brand_1" },
     { id: 2, title: "Brand 2", sort: "200", code: "brand_2" }
   ])
-
   const items = ref([
-    { type: "simple", id: 1, sku: "s1", title: "Product 1", regular_price: { currency: "USD", value: 27.12 }, image: "/images/1.png", brand: 1 },
-    { type: "simple", id: 2, sku: "s1", title: "Product 2", regular_price: { currency: "USD", value: 37.88 }, image: "/images/1.png", brand: 2 },
-    { type: "simple", id: 3, sku: "s1", title: "Product 3", regular_price: { currency: "USD", value: 39.77 }, image: "/images/1.png", brand: 2 }
+    { type: "simple", id: 1, sku: "s1", title: "Product 1", regular_price: { currency: "USD", value: 27.12 }, image: "/images/1.png", brand: 1, isOpen: false },
+    { type: "simple", id: 2, sku: "s1", title: "Product 2", regular_price: { currency: "USD", value: 37.88 }, image: "/images/1.png", brand: 2, isOpen: false },
+    { type: "simple", id: 3, sku: "s1", title: "Product 3", regular_price: { currency: "USD", value: 39.77 }, image: "/images/1.png", brand: 2, isOpen: false }
   ])
-
   const filteredBrand = ref(null)
   const filteredItems = computed(() => {
-    return filteredBrand.value ? items.value.filter(i => i.brand === filteredBrand.value) : items.value
+    return filteredBrand.value ? items.value.filter(item => item.brand === filteredBrand.value) : items.value;
   })
   const filterByBrand = (brand = null) => {
-    filteredBrand.value = brand
+    filteredBrand.value = brand;
+  }
+  const expandBox = id => {
+    filteredItems.value = filteredItems.value.map(filterItem => filterItem.isOpen && filterItem.id !== id ? {...filterItem, isOpen: false} : filterItem)
+    filteredItems.value = filteredItems.value.map(filterItem => filterItem.id === id ? {...filterItem, isOpen: !filterItem.isOpen} : filterItem)
   }
 </script>
 
@@ -30,10 +32,17 @@
     <div class="filter-wrap">
       <div class="filter-row">
         <FilterItem 
-          v-for="(item, id) in filteredItems"
-          :key="id"
+          v-for="filterItem in filteredItems"
+          :key="filterItem.id"
+          :filterItem="filterItem"
           @expand-box="expandBox"
         />
+        <!-- <div v-for="(item, id) in filteredItems" :key="id">
+          {{ item.title }} brand {{ item.brand }} ${{ item.regular_price.value }}
+          <div id="btn"> 
+            Add to cart
+          </div>
+        </div> -->
       </div>
     </div>
   </div>
--- src/components/FilterItem.vue
+++ src/components/FilterItem.vue
@@ -1,5 +1,4 @@
 <script setup>
-  import SvgIcon from './SvgIcon.vue'
   defineProps(['filterItem'])
   const emit = defineEmits(['expandBox']) // defining events to emit
   const handleClick = id => emit('expandBox', id) // emitting toggleDescription event with id attribute.
@@ -7,12 +6,12 @@
 
 <template>
   <div :class="['filter-item', {open: filterItem.isOpen}]" @click="() => handleClick(filterItem.id)">
-    <div :class="['description', {open: filterItem.isOpen}]">
-      <h2>{{ item.title }}</h2>
-      <h3>{{ item.brand }}</h3>
-      <h4>${{ item.regular_price.value }}</h4>
+    <div>
+      <h2>{{ filterItem.title }}</h2>
+      <h3>{{ filterItem.brand }}</h3>
+      <h4>${{ filterItem.regular_price.value }}</h4>
     </div>
-    <div :class="['add-btn', {open: filterItem.isOpen}]"> 
+    <div> 
       Add to cart
     </div>
   </div>
