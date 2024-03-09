607228a29424f01449e32a055fb9a2110970d3ac
Stephen McMahon <steve.mcmahons@gmail.com>
2024-03-08 21:06:24 -0500

    styles and data

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -2,17 +2,26 @@
   import { ref, computed } from 'vue'
   import FilterItem from './FilterItem.vue'
   const brand = ref([
-    { id: 1, title: "Brand 1", sort: "100", code: "brand_1" },
-    { id: 2, title: "Brand 2", sort: "200", code: "brand_2" },
-    { id: 3, title: "Brand 3", sort: "300", code: "brand_3" }
+    { id: 1, title: "Nike", sort: "100", code: "brand_1" },
+    { id: 2, title: "Adidas", sort: "200", code: "brand_2" },
+    { id: 3, title: "Apple", sort: "300", code: "brand_3" },
+    { id: 4, title: "Samsung", sort: "400", code: "brand_4" },
+    { id: 5, title: "Google", sort: "500", code: "brand_5" },
+    { id: 6, title: "Microsoft", sort: "600", code: "brand_6" }
   ])
   const items = ref([
-    { type: "simple", id: 1, sku: "s1", title: "Product 1", regular_price: { currency: "USD", value: 27.12 }, image: "product.jpg", brand: 1 },
-    { type: "simple", id: 2, sku: "s1", title: "Product 2", regular_price: { currency: "USD", value: 37.88 }, image: "product.jpg", brand: 2 },
-    { type: "simple", id: 3, sku: "s1", title: "Product 3", regular_price: { currency: "USD", value: 39.77 }, image: "product.jpg", brand: 2 },
-    { type: "simple", id: 4, sku: "s1", title: "Product 4", regular_price: { currency: "USD", value: 42.77 }, image: "product.jpg", brand: 3 },
-    { type: "simple", id: 5, sku: "s1", title: "Product 5", regular_price: { currency: "USD", value: 54.23 }, image: "product.jpg", brand: 3 },
-    { type: "simple", id: 6, sku: "s1", title: "Product 6", regular_price: { currency: "USD", value: 60.99 }, image: "product.jpg", brand: 3 }
+    { type: "Nike", id: 1, sku: "s1", title: "Bluetooth-enabled smart water bottle", regular_price: { currency: "USD", value: 27.12 }, image: "product.jpg", brand: 1 },
+    { type: "Adidas", id: 2, sku: "s1", title: "Bamboo cutting board set", regular_price: { currency: "USD", value: 37.88 }, image: "product.jpg", brand: 2 },
+    { type: "Adidas", id: 3, sku: "s1", title: "Portable solar charger for electronic devices", regular_price: { currency: "USD", value: 39.77 }, image: "product.jpg", brand: 2 },
+    { type: "Apple", id: 4, sku: "s1", title: "Resistance band set for home workouts", regular_price: { currency: "USD", value: 42.77 }, image: "product.jpg", brand: 3 },
+    { type: "Apple", id: 5, sku: "s1", title: "Himalayan salt lamp", regular_price: { currency: "USD", value: 54.23 }, image: "product.jpg", brand: 3 },
+    { type: "Apple", id: 6, sku: "s1", title: "Instant camera with film", regular_price: { currency: "USD", value: 60.99 }, image: "product.jpg", brand: 3 },
+    { type: "Samsung", id: 7, sku: "s1", title: "Electric wine bottle opener", regular_price: { currency: "USD", value: 77.12 }, image: "product.jpg", brand: 4 },
+    { type: "Samsung", id: 8, sku: "s1", title: "Silicone kitchen utensil set", regular_price: { currency: "USD", value: 87.88 }, image: "product.jpg", brand: 4 },
+    { type: "Google", id: 9, sku: "s1", title: "Memory foam neck pillow for travel", regular_price: { currency: "USD", value: 99.77 }, image: "product.jpg", brand: 5 },
+    { type: "Google", id: 10, sku: "s1", title: "Mini desktop vacuum cleaner", regular_price: { currency: "USD", value: 142.77 }, image: "product.jpg", brand: 5 },
+    { type: "Microsoft", id: 11, sku: "s1", title: "Essential oil diffuser with LED lights", regular_price: { currency: "USD", value: 154.23 }, image: "product.jpg", brand: 6 },
+    { type: "Microsoft", id: 12, sku: "s1", title: "Handheld milk frother for coffee beverages", regular_price: { currency: "USD", value: 260.99 }, image: "product.jpg", brand: 6 }
   ])
   const filteredBrand = ref(false)
   const filteredItems = computed(() => {
@@ -66,23 +75,22 @@
       padding: 2rem;
       text-align: center;
       span {
-        color: var(--color-heading);
+        color: var(--color-background);
         transition: all 0.25s ease-in-out;
         cursor: pointer;
         &:hover {
-          color: var(--color-text);
+          color: var(--color-background-mute);
         }
       }
       .filter-btn {
         margin: 10px 0;
-        padding: 10px;
+        padding: 5px;
         border-radius: 8px;
-        background: linear-gradient(145deg, #ffffff, #dfdfdf);
-        box-shadow: 4px 4px 2px #c1c1c1, -4px -4px 2px #ffffff;
+        background: linear-gradient(145deg, #ffffff, #aaaaaa);
         transition: all 0.25s ease-in-out;
         cursor: pointer;
         &:hover {
-          box-shadow: -5px -5px 10px #ffffff;
+          background: linear-gradient(145deg, #ececec, #ffffff);
         }
       }
     }
--- src/components/FilterItem.vue
+++ src/components/FilterItem.vue
@@ -6,8 +6,8 @@
   <div class="filter-item">
     <div class="product-info">
       <div class="product-title">{{ filterItem.title }}</div>
-      <div class="product-brand">{{ filterItem.brand }}</div>
-      <img alt="Product Image" src="../assets/product.jpg" />
+      <div class="product-brand">{{ filterItem.type }}</div>
+      <img alt="Product Image" :src="'/src/assets/' + filterItem.image" />
       <div class="product-price">${{ filterItem.regular_price.value }}</div>
     </div>
     <div class="cart-btn"> 
@@ -18,34 +18,64 @@
 
 <style lang="less">
   .filter-item {
+    position: relative;
     width: 200px;
     min-width: 200px;
     min-height: 200px;
+    max-height: 200px;
     margin: 1rem;
     padding: 1rem;
     border-radius: 10px;
     background: #f8f8f8;
-    box-shadow: 5px 5px 10px #dddddd, -5px -5px 10px #ffffff;
     transition: all 0.25s ease-in-out;
     animation-name: fade;
     animation-duration: 1s;
     cursor: pointer;
     &.active {
-      transform: scale(1.5);
+      transform: scale(1.75);
       z-index: 99;
+      box-shadow: 5px 5px 10px rgba(0,0,0,0.25), -5px -5px 10px rgba(0,0,0,0.25);
       cursor: default;
       .cart-btn {
         opacity: 1;
       }
     }
     .product-info {
+      width: auto;
+      .product-title {
+        font-size: 14px;
+        font-weight: bold;
+        line-height: 14px;
+      }
+      .product-brand {
+        font-size: 12px;
+      }
       img {
-        width: 75px;
-        margin: 0 auto;
+        width: 80px;
+        margin: auto;
+        display: block;
+      }
+      .product-price {
+        font-weight: bold;
+        font-size: 18px;
+        text-align: center;
+
       }
     }
     .cart-btn {
       opacity: 0;
+      position: absolute;
+      bottom: 3px;
+      left: 50%;
+      transform: translate(-50%, 0);
+      width: 65%;
+      text-align: center;
+      background-color: var(--color-heading);
+      font-size: 12px;
+      text-transform: uppercase;
+      font-weight: bold;
+      color: var(--color-background);
+      border-radius: 8px;
       transition: all 0.25s ease-in-out;
       cursor: pointer;
     }
